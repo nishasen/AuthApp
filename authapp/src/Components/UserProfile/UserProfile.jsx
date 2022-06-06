8e65c8bd1d9df9beb0032b51ee6d19b2a2fa7c78
nishasen <43262505+nishasen@users.noreply.github.com>
2022-06-07 02:34:42 +0530

    Added skeleton

--- authapp/src/Components/UserProfile/UserProfile.jsx
+++ authapp/src/Components/UserProfile/UserProfile.jsx
@@ -1,20 +1,25 @@
-import { Typography } from '@mui/material';
+import { Typography, Skeleton } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import './UserProfile.css';
 
 const UserProfile = () => {
-    const { user } = useSelector(state => state.auth)
+    const { user, loading } = useSelector(state => state.auth)
   return (
     <>
-    {user ? 
+    {loading ? 
+        <div className="user-profile">
+            <Skeleton animation="wave" variant="circular" width={60} height={60} />
+            <Skeleton animation="wave" variant="text" width={100}/>
+            <Skeleton animation="wave" variant="text" width={80}/>
+        </div>
+        :
         <div className="user-profile">
         <img src={user?.avatar} alt="user" className="profile profile-lg"/>
         <Typography variant="h5">{user?.firstname + " " + user?.lastname}</Typography>
         <Typography variant="body1" sx={{color: "grey"}}>{user?.email}</Typography>
         </div>
-        :
-        ""}
+        }
     </>
   )
 }
